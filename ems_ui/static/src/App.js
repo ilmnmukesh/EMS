8ebc492be5d90b0e537fd91ce85cff144e776a40
ilmnmukesh <ilmnmukesh@gmail.com>
2021-10-15 10:38:54 +0530

    initialize project

---
+++ ems_ui/static/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Dact from "./dact.svg";
+
+const App = () => {
+    return (
+        <div className="App">
+            <Dact className="Applogo" />
+            <div className="Apptext">Hello world</div>
+        </div>
+    );
+};
+export default App;
