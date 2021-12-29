08069e8b55c06389b23bb0a097308e53953628e4
Gowthaman21 <manogowtham211@gmail.com>
2021-12-29 20:19:51 +0530

    Student login bug fixed

--- ems_ui/static/src/components/header.jsx
+++ ems_ui/static/src/components/header.jsx
@@ -4,30 +4,31 @@ import { Link } from "react-router-dom";
 const Header = ({ current }) => {
     return (
         <>
-            <header id='header' className='fixed-top'>
-                <div className='container d-flex align-items-center justify-content-between'>
-                    <h1 className='logo'>
-                        <Link to='/studentHome' href='/'>
+            <header id="header" className="fixed-top">
+                <div className="container d-flex align-items-center justify-content-between">
+                    <h1 className="logo">
+                        <Link to="/studentHome" href="/">
                             EMSs
                         </Link>
                     </h1>
 
-                    <a href='index.html' className='logo'>
+                    <a href="index.html" className="logo">
                         <img
-                            src='assets/img/logo.png'
-                            alt=''
-                            className='img-fluid'
+                            src="assets/img/logo.png"
+                            alt=""
+                            className="img-fluid"
                         />
                     </a>
 
-                    <nav id='navbar' className='navbar'>
+                    <nav id="navbar" className="navbar">
                         <ul>
                             <li>
                                 <Link
-                                    to='/studentHome'
+                                    to="/studentHome"
                                     className={`nav-link scrollto ${
                                         current === "dashboard" && "active"
-                                    }`}>
+                                    }`}
+                                >
                                     Dashboard
                                 </Link>
                             </li>
@@ -37,7 +38,8 @@ const Header = ({ current }) => {
                                     className={`nav-link scrollto ${
                                         current === "enroll" && "active"
                                     }`}
-                                    to='/enrollment'>
+                                    to="/enrollment"
+                                >
                                     Enrollment
                                 </Link>
                             </li>
@@ -46,7 +48,8 @@ const Header = ({ current }) => {
                                     className={`nav-link scrollto ${
                                         current === "mark" && "active"
                                     }`}
-                                    to='/mark'>
+                                    to="/mark"
+                                >
                                     Attendance {"&"} Marks
                                 </Link>
                             </li>
@@ -55,7 +58,8 @@ const Header = ({ current }) => {
                                     className={`nav-link scrollto ${
                                         current === "fee" && "active"
                                     }`}
-                                    to='/fee'>
+                                    to="/fee"
+                                >
                                     Examination Fee
                                 </Link>
                             </li>
@@ -64,19 +68,18 @@ const Header = ({ current }) => {
                                     className={`nav-link scrollto ${
                                         current === "result" && "active"
                                     }`}
-                                    to='#contact'>
+                                    to="#contact"
+                                >
                                     Academic Result
                                 </Link>
                             </li>
                             <li>
-                                <Link
-                                    className='getstarted scrollto'
-                                    to='#about'>
+                                <Link className="getstarted scrollto" to="/">
                                     Log out
                                 </Link>
                             </li>
                         </ul>
-                        <i className='bi bi-list mobile-nav-toggle'></i>
+                        <i className="bi bi-list mobile-nav-toggle"></i>
                     </nav>
                 </div>
             </header>
